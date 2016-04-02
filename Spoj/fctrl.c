db7d0f6d1f9daba6e3c8705ebd2cc5c90f9a7a3c
Navneet <Navneet@Navneet-PC>
2016-04-03 00:46:33 +0530

    changes in fctrl.c

--- Spoj/fctrl.c
+++ Spoj/fctrl.c
@@ -1,10 +1,11 @@
 int fctrl(long int input){
-
+    
 	long int sum = 0;
 	int i ;
 	for(i = 5; (input/i) > 0; i = i*5 ){
 		sum = sum + input / i;
 	}
+        
 	printf("%ld\n",sum);
 	return 0;
 }
